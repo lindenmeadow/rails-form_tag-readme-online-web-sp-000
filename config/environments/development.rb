c2be68317e706f06395b99e6ac3366c8d2d9058f
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-02 18:44:49 +0000

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  class Application < Rails::Application
+    config.web_console.whitelisted_ips = '<IP address here>'
+  end
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
